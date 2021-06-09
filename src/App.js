a4bae509685eacd64d6bcc30e549935ae5463df2
OSintt <akumagamer123@gmail.com>
2021-06-09 11:38:16 -0500

    fixing responsive

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Slide from 'react-reveal/Slide';
 import Audio from './components/audio';
 
+
 function App() {
 
   return (
